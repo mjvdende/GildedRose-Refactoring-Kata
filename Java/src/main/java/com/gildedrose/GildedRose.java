c4c88d343889fcd7d8b35366d13baf6495208c7a
Maarten van den Ende <maarten@buildeleven.nl>
2020-11-09 20:24:30 +0100

    - finish refactor GildedRose

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,25 +12,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
-            // if normal or legendary
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    updateFor(items[i]).update(items[i]);
-            }
-
-            // if backstage
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                updateFor(items[i]).update(items[i]);
-            }
-
-            // if quality
-            if (items[i].name.equals("Aged Brie")){
-                updateFor(items[i]).update(items[i]);
-
-            }
-
-
+            updateFor(items[i]).update(items[i]);
         }
     }
 }
